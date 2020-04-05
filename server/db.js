b435bc647691aa0f53046ed4c2e2417c7a7bb83a
Tahzib <tssunesara@uh.edu>
2020-04-04 19:17:49 -0500

    updated db to query for user and authenticate user

--- server/db.js
+++ server/db.js
@@ -9,23 +9,13 @@ let pool = mysql.createPool({
 });
 
 
-//test a connection. Successful connection displays Connected to the MySQL server
-// in the console
-pool.getConnection(function(err, connection) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
- 
-  console.log('Connected to the MySQL server.');
+//to verify if employee user and password are correct when logging. It returns true/false 
+//if you need some help understanding https://www.mysqltutorial.org/mysql-nodejs/select/
+authenticateEmployee = function(data, callback){
 
-  connection.release();
-});
-
-
-//returns the names of all tables in database
-//includes alot of system tables as well
-testConnection = function(data, callback){
-  var sql = "SELECT table_name FROM information_schema.tables;"
+  // ? is a placeholder for the data passed into the authenticateEmployee function
+  // The placeholder will be substituted by the values of the array in sequence
+  var sql = "SELECT employee_id,pswd FROM zoo_schema.employee WHERE employee_id =?;"
   pool.getConnection(function(err, connection) {
     if(err) { console.log(err); callback(true); return; }
     
@@ -33,10 +23,46 @@ testConnection = function(data, callback){
     connection.query(sql, data, function(err, results) {
       connection.release();
       if(err) { console.log(err); callback(true); return; }
-      callback(false, results);
+
+      if(results[0].pswd == data[1]){  //check if the db password matches what was entered in login form
+        callback(false, true);   //returns true that the password was correct
+      }else{
+        callback(false, false);  //returns false that password is incorrect
+      }
     });
 
   });
 
 }
-module.exports.testConnection = testConnection;
+module.exports.authenticateEmployee = authenticateEmployee;
+
+
+
+//to verify if employee user and password are correct when logging. It returns true/false 
+//if you need some help understanding https://www.mysqltutorial.org/mysql-nodejs/select/
+authenticateCustomer = function(data, callback){
+
+  // ? is a placeholder for the data passed into the authenticateEmployee function
+  // The placeholder will be substituted by the values of the array in sequence
+  var sql = "SELECT email,pswd FROM zoo_schema.customer WHERE email=?;"
+  pool.getConnection(function(err, connection) {
+    if(err) { console.log(err); callback(true); return; }
+    
+
+    connection.query(sql, data, function(err, results) {
+      connection.release();
+      if(err) { console.log(err); callback(true); return; }
+
+      if(results[0].pswd == data[1]){  //check if the db password matches what was entered in login form
+        callback(false, true);   //returns true that the password was correct
+      }else{
+        callback(false, false);  //returns false that password is incorrect
+      }
+    });
+
+  });
+
+}
+module.exports.authenticateCustomer = authenticateCustomer;
+
+
