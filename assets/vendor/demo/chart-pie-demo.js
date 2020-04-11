0efd11f1a25c03d50b2ebb67b1186e4161842b79
thaohien97 <49213247+thaohien97@users.noreply.github.com>
2020-04-11 16:38:11 -0500

    updated manager page

--- assets/vendor/demo/chart-pie-demo.js
+++ assets/vendor/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Dolphin Show Ticket", "Giraffe Feeding Ticket", "Ferris Wheel Ticket", "Carousel Ticket", "Petting Zoo Ticket"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [10,25 , 20, 30, 15],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#F0F8FF', '#008B8B'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#F0F8FF', '#008B8B'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
