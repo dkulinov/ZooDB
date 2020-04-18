bb8f92af839ab76f1c83dc10ba0afa13ad1d5005
dkulinov <53202097+dkulinov@users.noreply.github.com>
2020-04-18 00:35:58 -0500

    Fixed the home route

--- app.js
+++ app.js
@@ -57,7 +57,36 @@ function assignEmployeeInfo(emp, dept, isM, isC, cb)
 // ----------------------------- ROUTES -------------------------------- //
 //default route will point to index.html which is the homepage
 
-app.get('/', (req, res) => res.sendFile(index.html));
+app.get('/', function(req, res){
+    
+    if(!req.session.user)
+    {
+        console.log('hello');
+        res.sendFile(path.join(__dirname,'/main.html'));
+    }
+    else if(req.session.user.role == "Customer")
+        res.redirect('/customerFrontPage');
+    else if(req.session.user.role == "Employee"){
+        if(req.session.user.isManager)
+        {
+            if(req.session.user.dept==9) 
+                res.redirect('/vetManager');
+            else if(req.session.user.isCareTaker) 
+                res.redirect('/caretakerManager');
+            else 
+                res.redirect('/managerFrontPage',);
+        }
+        else if(req.session.user.isCareTaker)
+        {
+                res.redirect('/caretaker');
+        }
+        else if(req.session.user.dept === 9){
+            res.redirect('/vet');
+        }
+        else
+            res.redirect('/regularEmployee');
+    }
+});
 
 
 app.post('/signup', function(req, res){
@@ -99,24 +128,8 @@ app.post('/employeeLogin', function(req, res){
                     req.session.user = new user(username, "Employee");
                     db.getEmployeeInfo(req.session.user, assignEmployeeInfo, function()
                     {
-                         if(req.session.user.isManager){
-                           if(req.session.user.dept==9) res.redirect('/vetManager');
-                           else if(req.session.user.isCareTaker) res.redirect('/caretakerManager');
-                           else res.redirect('/managerFrontPage',);
-                          }
-                        else if(req.session.user.isCareTaker){
-                           res.redirect('/caretaker');
-                        }
-                        else if(req.session.user.dept === 9){
-                           res.redirect('/vet');
-                        }
-                         /*else if(req.session.user.isManager)   // will be for managers only
-                             res.redirect('/managerFrontPage');*/
-                         else
-                             res.redirect('/regularEmployee'); // will be for regular employees                        
-                                         });
-
-                    
+                        res.redirect('/');             
+                    });
                 }else{
                   res.render('errorPage', {message: "Wrong username or password"});
                     
@@ -486,7 +499,7 @@ app.post('/alert', function(req, res)
     else if(req.session.user.isCareTaker)
     {
         // render caretaker report
-        db.getCareTakerAlerts(req.session.user, req.body.time, function(info)
+        db.getCareTakerAlerts(req.session.user, req.body.time, function(alerts, numHealthy, numSick, numPregnant, numDeceased)
         {
             res.render('caretaker_alerts.ejs', {data:[alerts, req.body.time, numHealthy, numSick, numPregnant,numDeceased]});
         });
